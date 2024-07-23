d572a8f63d5005125e4b30a51af3d5a4a15e4e5b
Binyamin Tamirat <biny01amin@gmail.com>
2024-07-23 20:42:10 +0300

    type fixed

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -45,7 +45,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
             return (
               <PaginationNumber
                 key={page}
-                href={createPageURL(page)}
+                href={createPageURL(index)}
                 page={page}
                 position={position}
                 isActive={currentPage === page}
